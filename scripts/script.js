335e1b3b11969947f6440e2ac596014ed2de0825
mohammad reza <mrghkh12@gmail.com>
2024-07-26 18:43:46 +0330

    feat(script.js): select element and set value

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ const $ = document
 
 let launchDate = new Date('Oct 24, 2024 00:00:00').getTime();
 
+let daysElem = $.querySelector('.days')
+let hoursElem = $.querySelector('.hours')
+let minutesElem = $.querySelector('.minutes')
+let secondsElem = $.querySelector('.seconds')
 
 let timer = setInterval(() => {
     let nowDate = new Date().getTime()
@@ -12,6 +16,10 @@ let timer = setInterval(() => {
     let distanceMinutes = Math.floor((distanceDate % (1000 * 60 * 60 )) / (1000 * 60 ))
     let distanceSeconds = Math.floor((distanceDate % (1000 * 60)) / 1000)
 
+    daysElem.innerHTML = distanceDays < 10 ? '0' + distanceDays : distanceDays 
+    hoursElem.innerHTML = distanceHours < 10 ? '0' + distanceHours : distanceHours
+    minutesElem.innerHTML = distanceMinutes < 10 ? '0' + distanceMinutes : distanceMinutes
+    secondsElem.innerHTML = distanceSeconds < 10 ? '0' + distanceSeconds : distanceSeconds
 
-    console.log(distanceDays,distanceHours,distanceMinutes,distanceSeconds);
+    
 }, 1000)
